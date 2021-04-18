a40cdc740ece93095dc5450e7dd3abfe4b7b8629
Matheus Frota <frota@4intelligence.com.br>
2021-04-18 14:36:38 -0300

    feat: Removendo função main, adicionando docstrings e implementado função que verifica se a base de dados está vazia.

--- src/get_match_history.py
+++ src/get_match_history.py
@@ -33,6 +33,7 @@ def save_matches(data, db_collection):
 
 
 def get_oldest_matches(db_collection):
+    """Função responsável por baixar os dados de partidas mais antigas."""
     qtd_documents = db_collection.count_documents({})
     min_match_id = db_collection.find_one(sort=[('match_id', 1)])['match_id']
     print(f"Até o momento foram coletados: {qtd_documents} documentos")
@@ -54,9 +55,18 @@ def get_oldest_matches(db_collection):
         )
 
 
-def get_newest_matches(db_collection):
+def checks_database_empty(db_collection):
+    """Verifica se a base de dados do mongodb está vazia"""
     qtd_documents = db_collection.count_documents({})
-    if qtd_documents > 0:
+    if qtd_documents == 0:
+        return True
+    return False
+
+
+def get_newest_matches(db_collection):
+    """Verifica se existe informação de alguma partida nova e atualiza a base de dados"""
+    qtd_documents = db_collection.count_documents({})
+    if not checks_database_empty(db_collection):
         list_match_id = [match['match_id'] for match in db_collection.find()]
         max_match_id_mongodb = db_collection.find_one(sort=[('match_id', -1)])['match_id']
     else:
@@ -66,7 +76,6 @@ def get_newest_matches(db_collection):
     print("Iniciando coleta dos dados")
     data = get_matches_batch()
     data = [match for match in data if match['match_id'] not in list_match_id]
-    # print(data)
     if len(data) == 0:
         print('Todos os recentes foram baixados')
         min_match_id = 0
@@ -90,18 +99,3 @@ def get_newest_matches(db_collection):
         print(
             f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Foram coletados: {qtd_documents} documentos"
         )
-
-
-def main(types):
-
-    mongodb_client = MongoClient(MONGODB_IP, MONGODB_PORT)
-    mongodb_database = mongodb_client["dota_raw"]
-
-    if types == 'oldest':
-        get_oldest_matches(mongodb_database['pro_match_history'])
-    elif types == 'newest':
-        get_newest_matches(mongodb_database['pro_match_history'])
-
-
-if __name__ == '__main__':
-    main()
